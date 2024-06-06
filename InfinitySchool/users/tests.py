b29e571e6de9d8f578244183d4a221880b206bff
VladiShm <114508981+VladiShm@users.noreply.github.com>
2024-06-06 17:11:07 +0500

    Добавлен тест на проверку авторизации

--- InfinitySchool/users/tests.py
+++ InfinitySchool/users/tests.py
@@ -1,13 +1,15 @@
 from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
-from users.forms import UserRegistrationForm
+from users.forms import UserRegistrationForm, UserLoginForm
 from users.models import User
 
-class UserRegistrationViewTestCase(TestCase):
+
+class UserTestCase(TestCase):
 
     def setUp(self):
-        self.path = reverse('users:register')
+        self.register_path = reverse('users:register')
+        self.login_path = reverse('users:login')
         self.data = {
             'first_name': 'Oleg',
             'last_name': 'Ivanov',
@@ -18,15 +20,26 @@ class UserRegistrationViewTestCase(TestCase):
         }
 
     def test_user_registration_get(self):
-        response = self.client.get(self.path)
+        response = self.client.get(self.register_path)
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertIsInstance(response.context['form'], UserRegistrationForm)
 
     def test_user_registration_post_success(self):
-        response = self.client.post(self.path, self.data)
+        response = self.client.post(self.register_path, self.data)
         username = self.data['username']
         # self.assertFalse(User.objects.filter(username=username).exists())
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertRedirects(response, reverse('users:login'))
         self.assertTrue(User.objects.filter(username=username).exists())
+
+    def test_user_login_after_registration(self):
+        self.client.post(self.register_path, self.data)
+        login_data = {
+            'username': self.data['username'],
+            'password': self.data['password1'],
+        }
+        response = self.client.post(self.login_path, login_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('index'))
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
 
