c8442d378646b3ec120fba1e8234d9ecc63cc248
VladiShm <114508981+VladiShm@users.noreply.github.com>
2024-06-29 18:53:25 +0500

    Кастомизация админки

--- InfinitySchool/school/admin.py
+++ InfinitySchool/school/admin.py
@@ -1,10 +1,51 @@
 from django.contrib import admin
-from school.models import Courses, CoursesTheory, PracticeTasks, Reviews
+from school.models import Courses, CoursesTheory, PracticeTasks, Reviews, Like
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(Courses)
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    fieldsets = (
+        (_('Основная информация'), {'fields': ('name', 'description', 'image')}),
+    )
 
-admin.site.register(CoursesTheory)
+class CoursesTheoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course')
+    search_fields = ('title', 'course__name')
+    list_filter = ('course',)
+    fieldsets = (
+        (_('Основная информация'), {'fields': ('course', 'title', 'theory')}),
+    )
 
-admin.site.register(PracticeTasks)
+class PracticeTasksAdmin(admin.ModelAdmin):
+    list_display = ('theory', 'description', 'difficulty')
+    search_fields = ('theory__title', 'description')
+    list_filter = ('theory', 'difficulty')
+    fieldsets = (
+        (_('Основная информация'), {'fields': ('theory', 'description', 'difficulty', 'answer')}),
+    )
 
-admin.site.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating', 'review', 'create_date')
+    search_fields = ('user__username', 'review')
+    list_filter = ('rating', 'create_date')
+    fieldsets = (
+        (_('Основная информация'), {'fields': ('user', 'rating', 'review')}),
+        (_('Дата создания'), {'fields': ('create_date',)}),
+    )
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'course', 'created_at')
+    search_fields = ('user__username', 'course__name')
+    list_filter = ('created_at',)
+    fieldsets = (
+        (_('Основная информация'), {'fields': ('user', 'course')}),
+        (_('Дата создания'), {'fields': ('created_at',)}),
+    )
+
+admin.site.register(Courses, CoursesAdmin)
+admin.site.register(CoursesTheory, CoursesTheoryAdmin)
+admin.site.register(PracticeTasks, PracticeTasksAdmin)
+admin.site.register(Reviews, ReviewsAdmin)
+admin.site.register(Like, LikeAdmin)
